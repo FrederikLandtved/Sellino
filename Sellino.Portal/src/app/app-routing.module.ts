9e609d39d3c5c918bd129b79b5a3e4d21664de8a
Frederik Landtved Jensen <frederiklandtved@hotmail.dk>
2023-08-12 09:54:34 +0200

    Add routing

--- Sellino.Portal/src/app/app-routing.module.ts
+++ Sellino.Portal/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { HomeComponent } from './pages/main/home/home.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
-const routes: Routes = [];
-
+const appRoutes: Routes = [
+  { path: '', component: MainComponent, 
+      children: [
+        { path: '', component: HomeComponent }
+      ]
+  },
+  { path: 'auth', component: AuthComponent, 
+      children: [
+        { path: '', component: LoginComponent },
+      ]
+  }
+]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Sellino.Portal/src/app/app.module.ts
+++ Sellino.Portal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { HomeComponent } from './pages/main/home/home.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    AuthComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

c28cc85699a78657f9e8a54c7281c2039e9855b8
Frederik Landtved Jensen <frederiklandtved@hotmail.dk>
2023-10-15 20:14:47 +0200

    Category page recursive tree WIP

--- Sellino.Portal/src/app/pages/main/categories/categories.component.ts
+++ Sellino.Portal/src/app/pages/main/categories/categories.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: Category[] = [];
 
+  ngOnInit(): void {
+    this.categories = 
+    [
+      { 
+        id: 1,
+        Name: "Tøj og sko", 
+        Children: [
+          { 
+            id: 2,
+            Name: "Sneakers",
+            Children: [
+              { id: 3, Name: "Nike", Children: [], showChildren: true }
+            ],
+            showChildren: true
+          },
+          { id: 4, Name: "Sandaler", Children: [], showChildren: true }
+        ],
+        showChildren: true 
+      }
+    ];
+  }
+
+  addCategory(category: Category) {
+    category.Children.push({ id: 5, Name: "Trøjer", Children: [], showChildren: false});
+  }
+
+  toggleChildren(category: Category): void {
+    category.showChildren = !category.showChildren;
+  }
+}
+
+class Category {
+  id: number = 0;
+  Name: string = "";
+  Children: Category[] = [];
+  showChildren: boolean = false;
 }

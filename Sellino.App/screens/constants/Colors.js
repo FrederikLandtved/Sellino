6fde0466441f0f339dbd128f0780dc8014e1f76b
Frederik Landtved Jensen <frederiklandtved@hotmail.dk>
2023-12-28 12:56:45 +0100

    Remove old app from project

--- Sellino.App/screens/constants/Colors.js
+++
@@ -1,7 +0,0 @@
-export const mainColors = {
-    headerColorPrimary: '#2E3D4A',
-    headerColorSecondary: '#30608C',
-    bgColorSecondary: '#E7E7E7',
-    bgAccentColor: '#19B959',
-    textOnAccentColor: '#ffffff'
-}
--- Sellino.App/screens/context/ThemeContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ThemeContext = React.createContext({
-    backgroundColor: 'blue',
-    setBackgroundColor: () => {}
-});
-
-export default ThemeContext;

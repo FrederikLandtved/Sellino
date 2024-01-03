277a1367b058d1811a00ef944f3810d181d19549
Frederik Landtved <32452239+FrederikLandtved@users.noreply.github.com>
2024-01-03 18:55:02 +0100

    Delete Sellino.Mobile/SellinoMobile directory

--- Sellino.Mobile/SellinoMobile/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { ProfileListItemComponent } from './components/profile-list-item/profile-list-item.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent}
-];
--- Sellino.Mobile/SellinoMobile/src/app/pipes/media.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MediaPipe } from './media.pipe';
-
-describe('MediaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MediaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
